6ce013a1cb50f832c24bdd7baf3f2f5dd791fec7
Debraj Manna <debraj@spotnana.com>
2022-03-12 18:47:47 +0530

    Use fetchLazy with jooq

--- src/main/java/com/tej/JooQDemo/BookService.java
+++ src/main/java/com/tej/JooQDemo/BookService.java
@@ -2,28 +2,45 @@ package com.tej.JooQDemo;
 
 import com.tej.JooQDemo.jooq.sample.model.Tables;
 import com.tej.JooQDemo.jooq.sample.model.tables.pojos.Book;
+import com.tej.JooQDemo.jooq.sample.model.tables.records.BookRecord;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.jooq.Cursor;
 import org.jooq.DSLContext;
+import org.jooq.ResultQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 public class BookService {
 
     @Autowired
     DSLContext context;
 
+    private final TransactionalRunner transactionRunner;
+
     public List<Book> getBooks(){
-       return context
-                .selectFrom(Tables.BOOK)
-                .fetchInto(Book.class);
+        final List<Book> books = new ArrayList<>();
+        ResultQuery<BookRecord> resQuery = context.selectFrom(Tables.BOOK).fetchSize(2);
+        transactionRunner.readOnlyTransaction(() -> {
+            try(final Cursor<BookRecord> bookRecords = resQuery.fetchLazy()) {
+                while (bookRecords.hasNext()) {
+                    List<Book> into = bookRecords.fetch().into(Book.class);
+                    System.out.println("Num Records: " + into.size());
+                    books.addAll(into);
+                }
+            }
+        });
+        return books;
     }
 
     public void insertBook(Book book){
         System.out.println(book.getTitle());
         context
-                .insertInto(Tables.BOOK, Tables.BOOK.TITLE, Tables.BOOK.AUTHOR)
-                .values(book.getTitle(), book.getAuthor())
-                .execute();
+            .insertInto(Tables.BOOK, Tables.BOOK.TITLE, Tables.BOOK.AUTHOR)
+            .values(book.getTitle(), book.getAuthor())
+            .execute();
     }
 }
---
+++ src/main/java/com/tej/JooQDemo/TransactionalRunner.java
@@ -0,0 +1,25 @@
+package com.tej.JooQDemo;
+
+import lombok.AllArgsConstructor;
+import lombok.var;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
+
+/** Provides support for DB transactions. All transactions run at serializable isolation level. */
+@Service
+@AllArgsConstructor
+public class TransactionalRunner {
+  private final PlatformTransactionManager manager;
+
+  public void readOnlyTransaction(Runnable fn) {
+    var template = new TransactionTemplate(manager);
+    template.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
+    template.execute(
+        transactionStatus -> {
+          fn.run();
+          return null;
+        });
+  }
+}
